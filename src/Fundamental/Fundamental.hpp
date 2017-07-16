3b4f0fa4684823564d38ba999011c175d5669266
mxmlnkn <mxmlnkn@github.de>
2017-07-16 18:35:09 +0200

    add numpy-like dumpData function

--- src/Fundamental/Fundamental.hpp
+++ src/Fundamental/Fundamental.hpp
@@ -3,9 +3,12 @@
 
 #include <cassert>
 #include <ctime>
-#include <iomanip>              // get_time
+#include <iomanip>              // get_time, setprecision, scientific
+#include <fstream>
+#include <limits>               // max10digits
 #include <list>
 #include <map>
+#include <stdexcept>
 #include <string>               // getline
 #include <sstream>
 #include <vector>
@@ -100,4 +103,52 @@ inline std::string replace
         i += to.length(); // Handles case where 'to' is a substring of 'from'
     }
     return str;
+}
+
+
+template< typename T_Prec >
+inline void dumpData
+(
+    std::string const & filePath,
+    std::map< std::string, std::vector< T_Prec > > const & data
+)
+{
+    std::ofstream file;
+    file.open( filePath );
+    if ( file.fail() )
+        throw std::invalid_argument( "Couldn't open file!" );
+
+    /* write out description / comment line : # t x v ... */
+    /* precision 2 means 3.1, 1 means 3, 3 means 3.14, 0 means 3, -1 means 3.14159 (for some weird reason) */
+    auto const prec  = std::numeric_limits< T_Prec >::max_digits10;
+    auto const width = prec + 8;   // "+p.pppe+999 "
+
+    file << "#";
+    for ( auto const & kv : data )
+        file << std::setw( width ) << kv.first;
+    file << "\n";
+
+    size_t iRow = 0u;
+    bool nonEmptyColumnFound = false;
+    do
+    {
+        nonEmptyColumnFound = false;
+        /* print each column */
+        for ( auto const & kv : data )
+        {
+            if ( iRow < kv.second.size() )
+            {
+                file << std::setw( width ) << std::scientific << std::setprecision( prec ) << kv.second[iRow];
+                nonEmptyColumnFound = true;
+            }
+            //else if ( std::numeric_limits< T_Prec > has_quiet_NaN )
+            //    file << std::setw( width ) << std::numeric_limits< T_Prec > quiet_NaN();
+            else
+                file << std::setw( width ) << " ";  // not really the best way, but I don't wanna add wrong data ...
+        }
+        file << "\n";
+        ++iRow;
+    } while ( nonEmptyColumnFound );
+
+    file.close();
 }
